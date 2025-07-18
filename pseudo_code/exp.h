78c140c3d97b33b1dafc716ed0e07fe27acb1412
ytcheng-lab <yutung.cheng.tw@gmail.com>
2025-07-18 23:01:41 +0800

    [Remove] Delete unused pseudo code

--- pseudo_code/exp.h
+++
@@ -1,4 +0,0 @@
-void exp_golden(float* in, float* out, int N);
-void exp_taylor3(float* in, float* out, int N);
-void softmax(float* in, float* out, float* buf, int N);
-void softmax_expthLUT1(float* in, float* out, float* buf, int N);
